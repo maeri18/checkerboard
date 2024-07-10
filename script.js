5c9f46da5bca193ca7cc89eb032d80489f1f8ff6
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-07-10 16:37:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,123 @@
-// selection handling
-let damier_taille = 3
-damier = document.getElementById("damier");
+damier = document.getElementById("damier"); 
+
 damier.addEventListener("click",function(event){
     clicked = event.target;
     if (clicked.tagName == "LI")
     {
-        selected = document.getElementsByClassName("selected");
-        pcolor = (  selected.classList.contains("pwhite"))?"white":"noir";
-        if(selected.length===0){
+        elem_selected = damier.getElementsByClassName("selected");
+        if(elem_selected.length === 0)
+        {
             clicked.setAttribute("class", clicked.getAttribute("class") + " selected");
         }
-        else{
-        possible_pos = possible_positions(pion_to_coord(selected[0]) ,false,pcolor);
-        console.log(possible_pos);
-        move_pion(selected[0],clicked);
-        selected[0].classList.remove("selected");
-        //clicked.setAttribute("class", clicked.getAttribute("class") + " selected");
-    }
-    }
-});
-function ajout_pions_ligne(row,color){
-    for (elem of row.children){
-        if (elem.getAttribute("class").split(" ")[0]=="white"){
-            elem.setAttribute("class", elem.getAttribute("class") + " p"+color);
+        else
+        {
+            for(elem of elem_selected)
+            {
+                classes = elem.className.split(" ");
+                let newClasses = "";
+                for(classname of classes)
+                {
+                    if(classname!="selected")
+                        {newClasses = newClasses +" "+classname;}
+                }
+                elem.setAttribute("class",newClasses);
+            }
+            clicked.setAttribute("class", clicked.getAttribute("class") + " selected");
         }
-    }
-}
-function ajout_pions_blancs(n_rows){
-    //n_rows<damier.children.length;
-damier = document.getElementById("damier").children;
-for (elem of damier){
-    if(n_rows==0){
-        break;
-    }
-    if (elem.tagName == "UL"){
-        ajout_pions_ligne(elem,"white");
-        n_rows--;
-    }
-}
-}
-function ajout_pions_noirs(n_rows){
-    //n_rows<damier.children.length;
-damier = document.getElementById("damier").children;
-for (i=damier.length-1; i>=0; i--){
-    elem=damier[i];
-    if(n_rows==0){
-        break;
-    }
-    if (elem.tagName == "UL"){
-        ajout_pions_ligne(elem,"noir");
-        n_rows--;
-    }
-}
-}
-function ajout_pions(){
-    ajout_pions_blancs(damier_taille);
-    ajout_pions_noirs(damier_taille);
-}
-ajout_pions();
-function move_pion(current, target){
-   let class_current = current.getAttribute("class").split(" ");
-   let class_target = target.getAttribute("class").split(" ");
-   let p_current = (class_current.indexOf("pwhite")!=-1)?"pwhite":(class_current.indexOf("pnoir")!=-1)?"pnoir":"";
-   let p_target = (class_target.indexOf("pwhite")!=-1)?"pwhite":(class_target.indexOf("pnoir")!=-1)?"pnoir":"";
-  //console.log("before",p_current,p_target);
-   if(p_target===""){
-    if(p_current!=""){
-        current.classList.remove(p_current);
-        target.classList.add(p_current);
+
         
     }
-   }
-   else{
-    if(p_target != p_current){
-        if(p_current!=""){
-            current.classList.remove(p_current);
-            target.classList.add(p_current);
-            target.classList.remove(p_target);
-        }
+})
+
+function ajout_pions(rows){
+    rows_occupied = (rows - 2)/2;
+    for (i=0;i<rows_occupied;i++)
+    {
+        row1 = damier.children[i];
         
+        
+        for(const cell of row1.children)
+        {
+            if(cell.className == "noir")
+            {
+                cell.setAttribute("class",cell.getAttribute("class")+" pnoir");
+            }
+        }
+        row2 = damier.children[rows-i-1];
+        for(cell of row2.children)
+            {
+                if(cell.className == "noir")
+                {
+                    cell.setAttribute("class",cell.getAttribute("class")+" pwhite");
+                }
+            }
     }
-   } 
-   //console.log("after",p_current,p_target);
-   
 }
 
-function possible_positions(selected, dame, color_pion){
-    let selected_x = selected[0];
-    let selected_y = selected[1];
-    if(dame){
-        return [];
+function move_pion(pos1, pos2){ //pos 1 and pos2 are coordinates [x,y] //arrays
+ let x1 = pos1[0];
+ let y1 = pos1[1];
+
+ let x2 = pos2[0];
+ let y2 = pos2[1];
+
+ let old_cell = damier.children[x1].children[y1];
+
+ let new_cell = damier.children[x2].children[y2];
+
+ oldCellClasses = old_cell.className.split(" ");
+ let classes = "";
+
+ for (classname of oldCellClasses)
+ {
+    if(classname === "pwhite" || classname === "pnoir")
+    {
+        pcolor = classname;
     }
     else{
-        if(color_pion=="noir")
-        {
-            return[[selected_x-1,selected_y+1],[selected_x-1,selected_y-1]];
-        }
-        else{
+        classes = classes +" "+ classname;
+    }
+ }
+ 
+ old_cell.setAttribute("class", classes);
+ new_cell.setAttribute("class", new_cell.getAttribute("class")+" "+pcolor);
 
-            return[[selected_x+1,selected_y+1],[selected_x+1,selected_y-1]]
+
+}
+
+function init_damier(rows){
+    ajout_pions(rows);
+}
+ 
+function string_to_board(board_string,size)
+{
+    for( i = 0; i < size; i++)
+    {
+        for( j = 0; j < size; j++)
+        {
+            damier.children[i].children[j].classList.remove("pwhite","pnoir");
+            if(board_string[i][j] == 'b')
+            {
+                damier.children[i].children[j].classList.add("pnoir");
+            }
+            else if (board_string[i][j] == 'w')
+            {
+                damier.children[i].children[j].classList.add("pwhite");
+            }
+            else {
+                ;
+            }
         }
     }
-   
 }
-function pion_to_coord(pion){
-    return [parseInt(pion.id[1]),parseInt(pion.id[2])]
-}
-/*
-function check_available(damier,selected, dame, color_pion){
-    let res;
-    let possible_pos = possible_positions(selected,dame,color_pion);
-    for (pos in possible_pos){
-        if
-    }
-}
-*/
-function play(damier,player){
-    return ;
-}
-function init(){
-    //a function to initialize all things. (ajout_pions...)
-}
+
+init_damier(8);
+
+
+move_pion([1,0],[7,7]);
+
+setTimeout(() => {
+    string_to_board(["ewewewew","wewewewe","ewewewew","eeeeeeee","eeeeeeee","bebebebe","ebebebeb","bebebebe"],8);
+    //console.log(damier.children[0].children[0]);
+}, 2000);
+    
